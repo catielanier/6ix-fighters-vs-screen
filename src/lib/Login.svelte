4ef07bdbf22e7aba75429bf019d6069bea13157e
Catie Lanier <hello@catielanier.ca>
2026-02-20 23:26:08 -0500

    create login

---
+++ src/lib/Login.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { setToken } from "../utils/tokenService";
+  import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+  import { firebase } from "../utils/firebase";
+  export let setLogin;
+  let email = "";
+  let password = "";
+
+  const submitLogin = async (e) => {
+    e.preventDefault();
+    const auth = getAuth(firebase);
+    signInWithEmailAndPassword(auth, email, password).then((user) => {
+      setToken(JSON.stringify(user));
+      setLogin();
+    });
+  };
+</script>
+
+<div class="login">
+  <form on:submit={submitLogin}>
+    <input type="email" bind:value={email} placeholder="Email address" />
+    <input type="password" bind:value={password} placeholder="Password" />
+    <button type="submit">Login</button>
+  </form>
+</div>
